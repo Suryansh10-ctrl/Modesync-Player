964461172c8aeba2ae7752e852248f8629544531
suryansh sharma <ss8737021@gmail.com>
2026-07-10 14:32:53 +0530

    commit changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,9 +16,6 @@ const songRoutes = require('./routes/song.routes')
 
 app.use('/api/auth', authRoutes);
 app.use('/api/songs', songRoutes);
-app.use(express.static(path.join(__dirname, '../public')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'))
-});
+app.use(express.static('./public'))
 
 module.exports = app
